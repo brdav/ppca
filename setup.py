4e34fe028e76f71146df13811e6574b574f659be
brdav <davidbruggemann@datascience.ch>
2024-08-28 11:38:09 +0200

    update pkg

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages, setup
 
 if __name__ == "__main__":
     setup(
-        name="em_ppca",
+        name="ppca",
         version="1.0",
         description="",
         author="brdav",
         author_email="",
         url="",
-        install_requires=["numpy>=1.26.4", "numba>=0.59.1"],
+        install_requires=["numpy>=1.26.4"],
         packages=find_packages(),
     )
